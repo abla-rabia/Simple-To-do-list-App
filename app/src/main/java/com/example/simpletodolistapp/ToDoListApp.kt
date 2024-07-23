90ddf7f29ffbaab0fb3fd792ec60fab67ff0719c
abla-rabia <la_rabia@esi.dz>
2024-07-23 03:24:49 +0100

    fixed the delete problem and finished the 1st version of the app

--- app/src/main/java/com/example/simpletodolistapp/ToDoListApp.kt
+++ app/src/main/java/com/example/simpletodolistapp/ToDoListApp.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,17 +19,23 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -38,6 +45,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.traceEventStart
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.motionEventSpy
@@ -52,6 +60,8 @@ data class toDo(
     val name:String,
     val done: MutableState<Boolean> = mutableStateOf(false)
 )
+@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun ToDoListApp(){
     val toDos = remember { mutableStateOf(listOf<toDo>()) }
@@ -73,9 +83,10 @@ fun ToDoListApp(){
                 .padding(top = 20.dp)
         ){
             items(toDos.value){
-                ToDo(todo = it, onDeleteClick = {})
+                item ->
+                ToDo(todo = item, onDeleteClick = {
+                    toDos.value=toDos.value-item})
             }
-
         }
         IconButton(
             onClick = { showAlert.value=true },
@@ -89,6 +100,7 @@ fun ToDoListApp(){
         }
         if (showAlert.value){
             AlertDialog(
+                modifier = Modifier.align(Alignment.Center),
                 onDismissRequest = { showAlert.value=false },
                 confirmButton = {
                 Button(onClick = {
@@ -102,18 +114,24 @@ fun ToDoListApp(){
                         contentColor = Color.White // Text color
                     ),
                     shape = RoundedCornerShape(16.dp),
-                    modifier = Modifier.padding(16.dp)
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.BottomCenter)
                     ) {
                     Text(text = "Add", style = TextStyle(fontFamily = poppinsFontFamily))
                 }
             },
                 text = {
                     Column {
-                        Text(text = "Add a to do : ", style = TextStyle(fontFamily = poppinsFontFamily, fontSize = 16.sp))
+                        Text(text = "Add a to do", style = TextStyle(fontFamily = poppinsFontFamily, fontSize = 24.sp))
+                        Spacer(modifier = Modifier.height(20.dp))
                         OutlinedTextField(
+                            colors =TextFieldDefaults.outlinedTextFieldColors(
+                                focusedBorderColor = Color(0xFF800080)
+                            ),
                             value = addTodoName,
                             onValueChange = {addTodoName=it},
-                            label = { Text(text = "Enter a to do", style = TextStyle(fontFamily = poppinsFontFamily)
+                            label = { Text(text = "Enter a to do", style = TextStyle(fontFamily = poppinsFontFamily), modifier = Modifier.alpha(0.5f)
                         )})
                     }
                 },
@@ -146,13 +164,18 @@ fun ToDo(todo:toDo,onDeleteClick:()->Unit){
         {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Checkbox(
-                    checked = todo.done.value, onCheckedChange = { todo.done.value=!todo.done.value})
+                    checked = todo.done.value,
+                    onCheckedChange = { todo.done.value=!todo.done.value},
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = Color(0xFF800080)
+                    )
+                )
                 Text(text = todo.name, style = TextStyle(
                     fontSize = 16.sp,
                     fontFamily = poppinsFontFamily))
             }
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Default.MoreVert, contentDescription = "")
+            IconButton(onClick = { onDeleteClick() }) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = "")
                 
             }
 
